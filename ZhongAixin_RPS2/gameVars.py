7e0fcdd44d52e198e550c08a03dba872f208e88c
Aixin Zhong <90532540+QuinnZhong@users.noreply.github.com>
2021-11-13 19:45:48 -0500

    delete

--- ZhongAixin_RPS2/gameVars.py
+++
@@ -1,9 +0,0 @@
-choices = ["rock", "paper", "scissors"]
-
-# player will be the weapon the player chooses via input
-
-player = False
-
-# these lives need to decrement when a player loses a round
-playerLives = 2
-computerLives = 2
